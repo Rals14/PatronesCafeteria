2eec4637770221c63417e82f88f88ed4e787e190
Juan Martínez <jmmartinezs2156@gmail.com>
2025-07-05 00:16:07 -0600

    :sparkles: add more decorators

---
+++ src/main/java/DecoratorBuilder/HierbaBuena.java
@@ -0,0 +1,34 @@
+package DecoratorBuilder;
+
+import BebidaBuilder.Bebida;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HierbaBuena extends BebidaDecorator {
+    public HierbaBuena(Bebida bebida){
+        super(bebida);
+        List<String> ingredientes = new ArrayList<String>(bebida.getIngredientes());
+        ingredientes.add("hierba buena");
+        bebida.setIngredientes(ingredientes);
+    }
+
+    @Override
+    public void setBase(String base) {
+
+    }
+
+    @Override
+    public void setIngredientes(List<String> ingredientes){;
+    }
+
+    @Override
+    public List<String> getIngredientes() {
+        return ingredientes;
+    }
+
+    @Override
+    public String toString(){
+        return bebida.toString();
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,13 @@
 import BebidaBuilder.Barista;
 import BebidaBuilder.Bebida;
 import BebidaBuilder.BebidaCafeBuilder;
+import BebidaBuilder.BebidaCapuccinoBuilder;
 import DecoratorBuilder.LecheDecorator;
 
 public class Main {
     public static void main(String[] args) {
         Barista barista = new Barista();
-        Bebida cafe = barista.hacerBebida(new BebidaCafeBuilder());
+        Bebida cafe = barista.hacerBebida(new BebidaCapuccinoBuilder());
 
         Bebida cafeConLeche = new LecheDecorator(cafe);
 
